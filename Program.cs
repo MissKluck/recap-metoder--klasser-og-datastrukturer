232c7856afa112b8410b489b14492ddbe70f1592
MissKluck <sigrid.b.kluck@gmail.com>
2024-10-18 10:31:22 +0200

    created person and implemented in main

--- Program.cs
+++ Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        // decimal mer presis, om vi skriver et program som skal gjøre trigonometri og bruke sinus, samt bruke i finans
+        // for vanlig programmering er double og int nok
+        // float er mindre presis igjen, mindre i størrelse
+        // long er større, men ikke nødvendig i vanlig data
+        // med de primitive dataklassene kan vi gjøre ganske mye
+
+
+        Person person = new Person(); //henter inn klassen person så vi kan bruke de funksjonene her
+        person.Name = "John Doe";
+        person.Age = 30;
+        person.Hobby = "coding";
+        
+        List<Person> someGenericTypeObject = new List<Person>(); //Da har vi en liste av generic type object som er person og alt som er inne i person
+        someGenericTypeObject.Add(person);
+
+        foreach (var people in someGenericTypeObject)
+        {
+            Console.WriteLine($"{people.Name} {person.Age} {person.Hobby}");
+        }
     }
 }
---
+++ classes/person.cs
@@ -0,0 +1,6 @@
+public class Person
+{
+    public string? Name;
+    public int Age;
+    public string? Hobby;
+}
